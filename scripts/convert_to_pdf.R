f348e0f4e7eb2e45eb2c69b056228eb550b9bc79
gsimchoni <giora@vfunction.com>
2020-02-10 09:35:34 +0200

    Adds pdfs

--- scripts/convert_to_pdf.R
+++ scripts/convert_to_pdf.R
@@ -1,12 +1,8 @@
 library(tidyverse)
 library(pagedown)
 
-# this should be run from parent Class_Slides directory
-unit_dir <- "u0_d01-intro/"
-html_filename <- str_c(str_sub(unit_dir, 1, str_length(unit_dir) - 1), ".html")
-
-# copy files from parent directory to unit directory
 copy_to_unit_dir <- function(file_name) file.copy(file_name, unit_dir)
+remove_from_unit_dir <- function(file_name) file.remove(str_c(unit_dir, "/", file_name))
 
 files_to_copy <- c(
   "slides.css",
@@ -15,6 +11,11 @@ files_to_copy <- c(
   "DSApps_logo_white.jpg"
 )
 
+# this should be run from parent Class_Slides directory
+unit_dir <- "u1_d01-tidyverse/"
+html_filename <- str_c(str_sub(unit_dir, 1, str_length(unit_dir) - 1), ".html")
+
+# copy files from parent directory to unit directory
 walk(files_to_copy, copy_to_unit_dir)
 
 # copy the libs directory to unit directory
@@ -24,8 +25,6 @@ file.copy("libs", unit_dir, recursive = TRUE)
 chrome_print(str_c(unit_dir, html_filename))
 
 # remove files
-remove_from_unit_dir <- function(file_name) file.remove(str_c(unit_dir, "/", file_name))
-
 walk(files_to_copy, remove_from_unit_dir)
 
 # remove libs local folder
